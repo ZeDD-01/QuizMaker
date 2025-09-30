f9e338da6ce668f18fdce84d05e7940a4ce5e250
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-09-30 14:01:38 +0200

    UI for user to start the quiz

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -6,9 +6,40 @@ namespace QuizMaker
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Quiz Maker!");
+        do
+        {
+            Console.WriteLine("Welcome to the Quiz Maker!");
+            Console.WriteLine("Please choose if you want to");
+            Console.WriteLine("1 - play an existing quiz ");
+            Console.WriteLine("2 - create a new quiz ");
+            Console.WriteLine("3 - Exit the program ");
+            Console.WriteLine("Your choice: ");
+
+            string choice = Console.ReadLine();
+
+            Console.WriteLine($"You chose {choice}");
+
+            if (choice == "1")
+            {
+                //Choose from Quiz List
+                break;
+            }
+            else if (choice == "2")
+            {
+                Quiz myQuiz = new Quiz();
+                break;
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("Goodbye");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Wrong input. Please try again.");
+            }
+        }while (true);
         
-        Quiz myQuiz = new Quiz();
     }
 }
 }
